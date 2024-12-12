d0e9f35ef9de381d7dde6143535e1c274e7c95b2
AmericanHuston <130685096+AmericanHuston@users.noreply.github.com>
2024-12-12 12:46:38 -0700

    Delete enhanced_clans directory

--- enhanced_clans/clans/init.lua
+++
@@ -1,5 +0,0 @@
-dofile(core.get_modpath("clans").."/spawning.lua")
-dofile(core.get_modpath("clans").."/clans.lua")
-dofile(core.get_modpath("clans").."/clans_commands.lua")
-dofile(core.get_modpath("clans").."/kiwi_sea_test.lua")
-dofile(core.get_modpath("clans").."/sacrifice.lua")
