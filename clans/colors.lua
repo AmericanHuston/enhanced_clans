648e7ce1312b6d438912e72b8ac5a17e40d970e4
= <=>
2025-08-27 17:33:42 -0600

    finished with the first batch of colors for the mod

--- clans/colors.lua
+++ clans/colors.lua
@@ -3,4 +3,8 @@ core.register_chatcommand("colorp",{
     local msg, to = param:match("^([%a%d_-]+) (.+)$")
     local gray = (100, 100, 100)
     local orange = (245, 61, 15)
+    local swamp_green = (41,74,43)
+    local dark_forest_green = (23, 74, 1)
+    local forest_green = (73, 184, 26)
+    local blue = (35, 42, 250)
 })
