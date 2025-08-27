2f418d0260abef16a599671e1e4b1e6e5ea8c96d
= <=>
2025-08-27 17:25:22 -0600

    made some colors

---
+++ clans/colors.lua
@@ -0,0 +1,6 @@
+core.register_chatcommand("colorp",{
+    local player = temp:get_player_name()
+    local msg, to = param:match("^([%a%d_-]+) (.+)$")
+    local gray = (100, 100, 100)
+    local orange = (245, 61, 15)
+})
