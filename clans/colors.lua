673f6b97cb860d331212506e0f8f684997f253cb
= <=>
2025-08-27 17:37:04 -0600

    reorganizing

--- clans/colors.lua
+++ clans/colors.lua
@@ -1,10 +1,12 @@
-core.register_chatcommand("colorp",{
-    local player = temp:get_player_name()
-    local msg, to = param:match("^([%a%d_-]+) (.+)$")
     local gray = (100, 100, 100)
     local orange = (245, 61, 15)
     local swamp_green = (41,74,43)
     local dark_forest_green = (23, 74, 1)
     local forest_green = (73, 184, 26)
     local blue = (35, 42, 250)
+core.register_chatcommand("colorp",{
+    privs = {eventadmin},
+    local player = temp:get_player_name()
+    local msg, to = param:match("^([%a%d_-]+) (.+)$")
+
 })
