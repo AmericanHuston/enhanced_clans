869b8698c73f436325e7ccab095be2965ef8bd06
= <=>
2025-08-27 18:00:07 -0600

    revisions

--- clans/colors.lua
+++ clans/colors.lua
@@ -1,12 +1,21 @@
-    local gray = (100, 100, 100)
-    local orange = (245, 61, 15)
-    local swamp_green = (41,74,43)
-    local dark_forest_green = (23, 74, 1)
-    local forest_green = (73, 184, 26)
-    local blue = (35, 42, 250)
+local colors = {
+     gray  = {r = 100, g = 100, b = 100}
+     orange            = {r = 245, g = 61,  b = 15}
+     swamp_green       = {r = 41,  g = 74,  b = 43}
+     dark_forest_green = {r = 23,  g = 74,  b = 1}
+     forest_green      = {r = 73,  g = 184, b = 26}
+     black             = {r = 0,   g = 0,   b = 0}
+     blue              = {r = 35,  g = 42,  b = 250}
+}
+
+    local color = {r = redValue, g = greenValue, b = blueValue}
 core.register_chatcommand("colorp",{
     privs = {eventadmin},
     local player = temp:get_player_name()
-    local msg, to = param:match("^([%a%d_-]+) (.+)$")
+    local player,color = param:match("^([%a%d_-]+) (.+)$")
+
+    player:set_nametag_attributes(color = color)
+    player:set_attribute("nametag_color", luanti.serialize(colors[param]))
+
 
 })
